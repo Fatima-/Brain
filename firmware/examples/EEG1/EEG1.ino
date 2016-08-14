6717557d48091fb64199ad9bb53bccfdf7e2ccf4
Fatima- <fatima_barrera@hotmail.es>
2016-08-14 18:04:18 -0500

    Update EEG1.ino

--- firmware/examples/EEG1/EEG1.ino
+++ firmware/examples/EEG1/EEG1.ino
@@ -1,1 +1,22 @@
-#TEST
+#include <Brain.h>
+
+///
+Brain brain (Serial);
+
+void setup() {    ////////// Inicializacion
+
+  Serial.begin(9600);
+  
+}
+
+void loop () {
+    /// Expect packets about one per second
+    /// FORMAT: signal strength, attention, meditation, delta, tetha, low alpha, low beta, high beta, low gamma, high gamma
+    ///////////////////////////////////////////////////////////
+    
+    if (brain.update()) {
+       Serial.println(brain.readErrors());
+       Serial.println(brain.readCSV());
+    }
+
+}
